fe8fa9415f91ee019fe5ac68650b955211c3cbaf
Tyronita <evan.oleary99@gmail.com>
2026-01-05 13:20:59 +0000

    record teleoperated dataset

---
+++ record_teleoperated_dataset.py
@@ -0,0 +1,144 @@
+from lerobot.cameras.opencv.configuration_opencv import OpenCVCameraConfig
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.datasets.utils import hw_to_dataset_features
+from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
+from lerobot.teleoperators.so101_leader.config_so101_leader import SO101LeaderConfig
+from lerobot.teleoperators.so101_leader.so101_leader import SO101Leader
+from lerobot.utils.control_utils import init_keyboard_listener
+from lerobot.utils.utils import log_say
+from lerobot.utils.visualization_utils import init_rerun
+from lerobot.scripts.lerobot_record import record_loop
+from lerobot.processor import make_default_processors
+from dotenv import load_dotenv
+import os, datetime
+
+load_dotenv()
+
+
+# ===================== ENV VARS =====================
+HF_USERNAME = os.getenv("HF_USERNAME")
+HF_ACCESS_TOKEN = os.getenv("HF_ACCESS_TOKEN")
+
+if HF_USERNAME is None or HF_ACCESS_TOKEN is None:
+    raise RuntimeError(
+        "Missing environment variables."
+        "Please set HF_USERNAME and HF_ACCESS_TOKEN."
+    )
+
+# ===================== CONSTANTS =====================
+
+HF_DATASET_NAME = "gertrude_pick_and_place_" + str(datetime.datetime.now()).replace(".", "_").replace(" ", "_").replace(":", "_")
+
+NUM_EPISODES = 5
+FPS = 26
+EPISODE_TIME_SEC = 60
+RESET_TIME_SEC = 10
+TASK_DESCRIPTION = "My task description"
+
+camera_config = {
+    "gripper_cam": OpenCVCameraConfig(
+        index_or_path=1, # use ./outputs/captured_images -> to derive this
+        width=640,
+        height=480,
+        fps=FPS,
+        fourcc="YUY2",
+    ),
+    # "exocentric_cam": OpenCVCameraConfig(
+    #     index_or_path=2,
+    #     width=640,
+    #     height=480,
+    #     fps=30,               # allowed to differ
+    #     fourcc="YUY2",
+    # ),
+}
+
+robot_config = SO101FollowerConfig(
+    id="gertrude",
+    cameras=camera_config,
+    port="COM6",
+)
+
+teleop_config = SO101LeaderConfig(
+    id="jensen",
+    port="COM5",
+)
+
+# Initialize the robot and teleoperator
+robot = SO101Follower(robot_config)
+teleop = SO101Leader(teleop_config)
+
+# Configure the dataset features
+action_features = hw_to_dataset_features(robot.action_features, "action")
+obs_features = hw_to_dataset_features(robot.observation_features, "observation")
+dataset_features = {**action_features, **obs_features}
+
+# Create the dataset
+dataset = LeRobotDataset.create(
+    repo_id=f"{HF_USERNAME}/{HF_DATASET_NAME}",
+    fps=FPS,
+    features=dataset_features,
+    robot_type=robot.name,
+    use_videos=True,
+    image_writer_threads=4,
+)
+
+# Initialize the keyboard listener and rerun visualization
+_, events = init_keyboard_listener()
+init_rerun(session_name="recording")
+
+# Connect the robot and teleoperator
+robot.connect()
+teleop.connect()
+
+# Create the required processors
+teleop_action_processor, robot_action_processor, robot_observation_processor = make_default_processors()
+
+episode_idx = 0
+while episode_idx < NUM_EPISODES and not events["stop_recording"]:
+    log_say(f"Recording episode {episode_idx + 1} of {NUM_EPISODES}")
+
+    record_loop(
+        robot=robot,
+        events=events,
+        fps=FPS,
+        teleop_action_processor=teleop_action_processor,
+        robot_action_processor=robot_action_processor,
+        robot_observation_processor=robot_observation_processor,
+        teleop=teleop,
+        dataset=dataset,
+        control_time_s=EPISODE_TIME_SEC,
+        single_task=TASK_DESCRIPTION,
+        display_data=True,
+    )
+
+    # Reset the environment if not stopping or re-recording
+    if not events["stop_recording"] and (episode_idx < NUM_EPISODES - 1 or events["rerecord_episode"]):
+        log_say("Reset the environment")
+        record_loop(
+            robot=robot,
+            events=events,
+            fps=FPS,
+            teleop_action_processor=teleop_action_processor,
+            robot_action_processor=robot_action_processor,
+            robot_observation_processor=robot_observation_processor,
+            teleop=teleop,
+            control_time_s=RESET_TIME_SEC,
+            single_task=TASK_DESCRIPTION,
+            display_data=True,
+        )
+
+    if events["rerecord_episode"]:
+        log_say("Re-recording episode")
+        events["rerecord_episode"] = False
+        events["exit_early"] = False
+        dataset.clear_episode_buffer()
+        continue
+
+    dataset.save_episode()
+    episode_idx += 1
+
+# Clean up
+log_say("Stop recording")
+robot.disconnect()
+teleop.disconnect()
+dataset.push_to_hub()
